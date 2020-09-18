51f0ae456a18d4296bd46f9e470da40506b05918
Josh Riley <jriley@tfdidesign.com>
2020-09-18 19:38:22 +0100

    Initial data and reading CSV.

---
+++ script.R
@@ -0,0 +1,4 @@
+# Import Data
+# dataFolder <- "/Users/jriley/Documents/data"
+# fileList <- list.files(path = dataFolder, pattern = "*.csv")
+caseData <- read.csv('D:/Dev/R Learning/COVID-19-Stats/sep18Cases.csv', header = TRUE)
