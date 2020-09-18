4462fa14b0b7f7dbbfd9c01a6326064c5a75e182
Josh Riley <jriley@tfdidesign.com>
2020-09-18 23:39:14 +0100

    Graph correctly shows COVID case data for number of new confirmed cases per day.

--- script.R
+++ script.R
@@ -1,4 +1,11 @@
 # Import Data
 # dataFolder <- "/Users/jriley/Documents/data"
 # fileList <- list.files(path = dataFolder, pattern = "*.csv")
+library(ggplot2)
+
 caseData <- read.csv('D:/Dev/R Learning/COVID-19-Stats/sep18Cases.csv', header = TRUE)
+
+#View(caseData, "Case Data")
+
+p1 <- ggplot() + geom_line(aes(y = new, x = day), data = caseData)
+p1 + labs(title = "COVID-19 England Case Statistics", x = "Day", y = "Cases", caption = "Data from HM Government")
