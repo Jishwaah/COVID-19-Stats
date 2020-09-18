79891a292541df9eafbe7f35952688354250c479
Josh Riley <jriley@tfdidesign.com>
2020-09-18 23:53:58 +0100

    Deaths data is now reviewed and plotted.

--- script.R
+++ script.R
@@ -4,8 +4,13 @@
 library(ggplot2)
 
 caseData <- read.csv('D:/Dev/R Learning/COVID-19-Stats/sep18Cases.csv', header = TRUE)
+deathData <- read.csv('D:/Dev/R Learning/COVID-19-Stats/sep18Deaths.csv', header = TRUE)
 
 #View(caseData, "Case Data")
 
 p1 <- ggplot() + geom_line(aes(y = new, x = day), data = caseData)
 p1 + labs(title = "COVID-19 England Case Statistics", x = "Day", y = "Cases", caption = "Data from HM Government")
+
+p2 <- ggplot() + geom_line(aes(y = newDeaths, x = day), data = deathData)
+p2 + labs(title = "COVID-19 England Death Statistics", x = "Day", y = "Deaths", caption = "Data from HM Government")
+
